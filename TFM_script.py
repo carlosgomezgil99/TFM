c049be20f26dfbbc2cfb0677532f9e4b65eef084
carlosgomezgil99 <carlosgomezgil@outlook.es>
2022-06-04 13:02:45 +0200

    subida TFM_script

---
+++ TFM_script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+df=pd.read_excel('data/CMBD_6_20181217-135856.xlsx')
+df_calidad_aire_08=pd.read_csv('data/datos08.csv',sep=';')
+df_calidad_aire_09=pd.read_csv('data/datos09.csv',sep=';')
+df_calidad_aire_10=pd.read_csv('data/datos10.csv',sep=';')
+df_calidad_aire_11=pd.read_csv('data/datos11.csv',sep=';')
+df_calidad_aire_12=pd.read_csv('data/datos12.csv',sep=';')
+df_calidad_aire_13=pd.read_csv('data/datos13.csv',sep=';')
+df_calidad_aire_14=pd.read_csv('data/datos14.csv',sep=';')
+df_calidad_aire_15=pd.read_csv('data/datos15.csv',sep=';')
